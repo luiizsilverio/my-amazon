7816093a747fd52aa62fdbe1a5817bb8524530e8
luiizsilverio <luiiz.silverio@gmail.com>
2022-07-08 17:18:39 -0300

    Login form

--- utils/Store.js
+++ utils/Store.js
@@ -1,11 +1,12 @@
 import { createContext, useReducer } from "react";
+import Cookies from "js-cookie";
 
 export const Store = createContext();
 
 const initialState = {
-  cart: {
-     cartItems: []
-  }
+  cart: Cookies.get('my-amazon:cart')
+    ? JSON.parse(Cookies.get('my-amazon:cart'))
+    : { cartItems: [] }
 }
 
 function reducer(state, action) {
@@ -23,12 +24,14 @@ function reducer(state, action) {
         } else {
           cartItems = [...state.cart.cartItems, newItem]
         }
+        Cookies.set('my-amazon:cart', JSON.stringify({ ...state.cart, cartItems }));
         return { ...state, cart: { ...state.cart, cartItems } }
       }
       case 'CART_REMOVE_ITEM': {
         const cartItems = state.cart.cartItems.filter(
           (item) => item.slug !== action.payload.slug
         )
+        Cookies.set('my-amazon:cart', JSON.stringify({ ...state.cart, cartItems }));
         return { ...state, cart: { ...state.cart, cartItems } }
       }
       default:
